c57c32f53c1915f42f405109bd2547890c2b9ada
Frank Hunleth <fhunleth@troodon-software.com>
2014-02-09 14:27:25 -0500

    Pull in ps update to subprocess

--- src/subprocess.erl
+++ src/subprocess.erl
@@ -1,6 +1,30 @@
+%% The MIT License (MIT)
+%%
+%% Copyright (c) 2013 Frank Hunleth
+%%
+%% Permission is hereby granted, free of charge, to any person
+%% obtaining a copy of this software and associated documentation
+%% files (the "Software"), to deal in the Software without
+%% restriction, including without limitation the rights to use, copy,
+%% modify, merge, publish, distribute, sublicense, and/or sell copies
+%% of the Software, and to permit persons to whom the Software is
+%% furnished to do so, subject to the following conditions:
+%%
+%% The above copyright notice and this permission notice shall be
+%% included in all copies or substantial portions of the Software.
+%%
+%% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+%% EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+%% MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+%% NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+%% BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+%% ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+%% CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+%% SOFTWARE.
+
 -module(subprocess).
 
--export([run/1, run/2, run/3, cmdpp/1]).
+-export([run/1, run/2, run/3, cmdpp/1, ps/0]).
 
 % Run the specified executable and return ok on success
 -spec run(string()) -> ok | {error, non_neg_integer()}.
@@ -45,3 +69,24 @@ cmdpp(CmdLine) ->
     lists:foreach(fun(A) -> io:format("~s~n", [A]) end,
 		  string:tokens(Output, "\n")),
     ok.
+
+psstat(PidDir) ->
+    {ok, Fd} = file:open(PidDir ++ "/stat", [read]),
+    {ok, StatLine} = file:read_line(Fd),
+    file:close(Fd),
+    Fields = string:tokens(StatLine, " ()"),
+    { list_to_integer(hd(Fields)),
+      lists:nth(2, Fields) }.
+
+ps() ->
+    {ok, Files} = file:list_dir("/proc"),
+    lists:foldl(fun(Filename, A) ->
+			try list_to_integer(Filename) of
+			    _LinuxPid ->
+				[psstat("/proc/" ++ Filename)|A]
+			catch
+			    error:_ -> A
+			end
+		end,
+		[],
+		Files).
